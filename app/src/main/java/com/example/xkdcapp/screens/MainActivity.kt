a3eb807950f414420f42669309bba53933d22cd4
Mats Tveit <Mats.Andre.Tveit.Leirmo@dnb.no>
2021-05-03 22:00:30 +0200

    added navigation and set MainActivity in manifest

--- app/src/main/java/com/example/xkdcapp/screens/MainActivity.kt
+++ app/src/main/java/com/example/xkdcapp/screens/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.xkdcapp.screens
 
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import com.example.xkdcapp.R
 import com.example.xkdcapp.screens.common.activities.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -9,9 +12,17 @@ import kotlinx.android.synthetic.main.activity_main.*
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
 
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        navController = findNavController(this, R.id.nav_host_fragment)
+        setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
--- app/src/main/java/com/example/xkdcapp/screens/comiclist/ComicListViewModel.kt
+++ app/src/main/java/com/example/xkdcapp/screens/comiclist/ComicListViewModel.kt
@@ -3,5 +3,5 @@ package com.example.xkdcapp.screens.comiclist
 import com.example.xkdcapp.screens.common.viewModel.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 
-@HiltViewModel
+
 class ComicListViewModel : BaseViewModel()
